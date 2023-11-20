38f98de544befb08fe214b98333711a8e5fca0a7
Paul Dudrey <pdudrey@gmail.com>
2023-11-20 12:06:24 -0800

    Updated navigation

--- scripts/content.js
+++ scripts/content.js
@@ -1,56 +1,35 @@
-let pageHeading = document.getElementById('pageHeading');
-let mainContent = document.getElementById('mainContent');
 let aboutMe = document.getElementById('aboutMe');
 let experience = document.getElementById('experience');
 let pageSubHeading = document.getElementById('pageSubHeading');
 let pageTagline = document.getElementById('pageTagline');
+let aboutMeNav = document.getElementById("aboutMeNav");
+let experienceNav = document.getElementById("experienceNav");
 
-
-function loadContent(pageHeadingText, pageSubHeadingText, pageTaglineText, mainContentText){
-    pageHeading.innerText = pageHeadingText;
-    pageSubHeading.innerText = pageSubHeadingText;
-    pageTagline.innerHTML = pageTaglineText;
-    mainContent.innerHTML = mainContentText;
-}
-
-// window.onload = function(){
-//     loadAboutMe();
-// };
 
 
 function loadAboutMe(){ 
     experience.classList.add("d-none");
-
     aboutMe.classList.remove("d-none");
-    pageHeading.innerText = "About Me";
     pageSubHeading.innerText = "Software Engineering Leader";
     pageTagline.classList.remove("d-none");
-
-    
-    // const aboutMeTagLine = "<div class=\"col-lg-4\"><p class=\"text-center lead\">simplicity out of complexity</p></div>"
-    // + "<div class=\"col-lg-4\"><p class=\"text-center lead\">simplicity out of complexity</p></div>"
-    // + "<div class=\"col-lg-4\"><p class=\"text-center lead\">simplicity out of complexity</p></div>"
-    
-    // loadContent("About Me", "Software Engineering Leader", aboutMeTagLine, aboutMeContent);
+    experienceNav.classList.remove("active");
+    aboutMeNav.classList.add("active");
+    collapseNavbar();
 }
 
 
 function loadExperience(){
-
     aboutMe.classList.add("d-none");
-
     experience.classList.remove("d-none");
-    pageHeading.innerText = "Experience";
     pageSubHeading.innerText = "Work History";
     pageTagline.classList.add("d-none");
+    aboutMeNav.classList.remove("active");
+    experienceNav.classList.add("active");
+    collapseNavbar();
 
 }
 
-function loadInterests(){
-
-}
-
-function loadContact(){
-
+function collapseNavbar(){
+    document.getElementById('navbarSupportedContent').classList.remove("show");
 }
 
